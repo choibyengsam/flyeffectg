78b5b38349e45a94d561f0157d8c9670e9ee7d7c
hiddencity <87620798+choibyengsam@users.noreply.github.com>
2025-04-20 03:59:12 +0900

    Update mdbconn.py

--- DB/mdbconn.py
+++ DB/mdbconn.py
@@ -5,7 +5,8 @@ import os
 class SqliteDB():
     def __init__(self, db):
         self.BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-        self.conn = sqlite3.connect(self.BASE_DIR + '\\' + db)
+        db_path = os.path.join(self.BASE_DIR, db)
+        self.conn = sqlite3.connect(db_path)
         self.c = self.conn.cursor()
 
     def GetBirth(self, year, month, day):
@@ -15,11 +16,11 @@ class SqliteDB():
 
     def GetTime(self, time):
         jitime = [('子','자'),('丑','축'),('寅','인'),('卯','묘'),('辰','진'),('巳','사'),('午','오'),('未','미'),('申','신'),('酉','유'),('戌','술'),('亥','해')]
-        if(time>23 or time<0):
+        if(time > 23 or time < 0):
             return ''
-        ptime = time+1
-        if(ptime==24): ptime=0
-        itime = (int)(ptime/2)
+        ptime = time + 1
+        if(ptime == 24): ptime = 0
+        itime = int(ptime / 2)
         return jitime[itime]
 
     def Close(self):
